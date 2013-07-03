5ea765f727cae531127e4a56bd3f82d35ab9b9e9
leni-kirilov <leni.kirilov@gmail.com>
2013-07-03 21:29:03 +0300

    All events are reduced to only "All public events"

--- bg.su.fmi.st.calendar.model/src/bg/su/fmi/st/calendar/servlet/events/ViewAllEventsTableServlet.java
+++ bg.su.fmi.st.calendar.model/src/bg/su/fmi/st/calendar/servlet/events/ViewAllEventsTableServlet.java
@@ -1,15 +1,33 @@
 package bg.su.fmi.st.calendar.servlet.events;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import bg.su.fmi.st.calendar.model.entities.Event;
 
 @SuppressWarnings("serial")
-public class ViewAllEventsTableServlet extends AbstractEventsTableServlet {
+public class ViewAllEventsTableServlet extends ViewMyEventsTableServlet {
 
 	@Override
 	protected List<Event> getEventsForDisplay() {
-		return eventsDAO.getEvents();
+		List<Event> allEvents = eventsDAO.getEvents();
+
+		List<Event> publicEvents = new ArrayList<Event>();
+		for (Event e : allEvents) {
+			if (e.getType().equals("public")) {
+				publicEvents.add(e);
+			}
+		}
+
+		List<Event> myEvents = super.getEventsForDisplay();
+
+		Set<Event> uniqueEvents = new HashSet<Event>();
+		uniqueEvents.addAll(publicEvents);
+		uniqueEvents.addAll(myEvents);
+
+		return new ArrayList<Event>(uniqueEvents);
 	}
 
 	@Override
